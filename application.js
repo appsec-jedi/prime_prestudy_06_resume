fa7c1179f0b520e14fe19a1572f6a78d8498f23f
jacobjv25 <jacobjv25@gmail.com>
2016-12-20 06:17:41 +0900

    Update application.js

--- application.js
+++ application.js
@@ -9,4 +9,5 @@ $(document).ready(function(){
 var user = prompt("Hey, friend! Welcome to my page! What can I call you?");
 alert("Thanks for stopping by, " + user + "! Click on the button on the next page to learn more about me");
 
-setTimeout(function() { alert("You've been here a while, feel free to connect with me on LinkedIn and start a conversation"); }, 60000);
+setTimeout(function()
+  { alert("You've been here a while, " + user + ". feel free to connect with me on LinkedIn and start a conversation"); }, 60000);
