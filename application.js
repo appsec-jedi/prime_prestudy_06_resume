945c51d5f0378e60d8e2ff07db36edf0f3dfdcee
jacobjv25 <jacobjv25@gmail.com>
2017-01-03 08:27:42 -0600

    Update application.js

--- application.js
+++ application.js
@@ -11,3 +11,21 @@ alert("Thanks for stopping by, " + user + "! Click on the button on the next pag
 
 setTimeout(function()
   { alert("You've been here a while, " + user + ". feel free to connect with me on LinkedIn and start a conversation"); }, 60000);
+
+function openTab(evt, tabName) {
+
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+          tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+          tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
